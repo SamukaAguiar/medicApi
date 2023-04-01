02825d9d1edadeca512866d913ef7e8e202e088c
samukaaguiar <samuel@linqcode.com.br>
2023-04-01 18:27:58 -0300

    Initial commit

---
+++ src/main/java/med/voll/medicApi/MedicApiApplication.java
@@ -0,0 +1,13 @@
+package med.voll.medicApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/medicApi/MedicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.medicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
