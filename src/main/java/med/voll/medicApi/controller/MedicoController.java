469c1f02158facc227d73d7686d49e45e2540b0e
samukaaguiar <samuel@linqcode.com.br>
2023-04-16 16:35:30 -0300

    Add paciente

--- src/main/java/med/voll/medicApi/controller/MedicoController.java
+++ src/main/java/med/voll/medicApi/controller/MedicoController.java
@@ -5,11 +5,11 @@ import jakarta.validation.Valid;
 import med.voll.medicApi.medico.Medico;
 import med.voll.medicApi.medico.MedicoRepository;
 import med.voll.medicApi.medico.Medicos;
+import med.voll.medicApi.medico.MedicosDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,6 +22,11 @@ public class MedicoController {
     @Transactional
     public void insert(@RequestBody @Valid Medicos medico){
         repository.save(new Medico(medico));
+    }
+
+    @GetMapping
+    public List<MedicosDTO> listar(){
+        return repository.findAll().stream().map(MedicosDTO::new).toList();
     }
 
 }
